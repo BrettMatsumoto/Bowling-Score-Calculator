5d71427073af543e77622159dc701ee208c28aea
Brett <brett_matsumoto@yahoo.com>
2019-04-25 09:24:09 -1000

    attempted to make test, test not working

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const expect = chai.expect;
+const should = chai.should();
+
+describe('bowlingScoreCalculator', function(){
+  it('should be a function', function(){
+    expect(bowlingScoreCalculator).to.be.a('function');
+  })
+})
